f89ca047ae6415b828930678851d006db29089a2
web-xzy <webxzy@163.com>
2016-09-22 11:45:11 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,34 @@
+var data = {
+        title: '标题^_^',
+        items: [{
+            text: 'title1',
+            img: './imgs/first.png',
+            url: 'http://www.baidu.com'
+        },{
+            text: 'title2',
+            img: './imgs/second.png',
+            url: 'http://www.baidu.com'
+        },{
+            text: 'title3',
+            img: './imgs/third.png',
+            url: 'http://www.baidu.com'
+        },{
+            text: 'title4',
+            img: './imgs/fourth.png',
+            url: 'http://www.baidu.com'
+        },{
+            text: 'title5',
+            img: './imgs/fifth.png',
+            url: 'http://www.baidu.com'
+        },{
+            text: 'title6',
+            img: './imgs/third.png',
+            url: 'http://www.baidu.com'
+        },{
+            text: 'title7',
+            img: './imgs/second.png',
+            url: 'http://www.baidu.com'
+        }],
+        helpText: '帮助文字：政策流程随时间更新，请关注HR门户内容变化：',
+        link: 'duhr.baidu.com'
+    }
